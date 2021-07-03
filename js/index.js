12e9487e2ad370c63a545f3103068939194dcf17
JosueCombatSerafim <wrj.serafim@bol.com.br>
2021-07-03 10:08:06 -0300

    feat: add anchor menu and footer

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var $doc = $("html, body");
+$(".navbar_list--link").click(function () {
+  $doc.animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top,
+    },
+    500
+  );
+  return false;
+});
